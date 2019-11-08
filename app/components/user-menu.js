d5aa51ff13d80a25e227f871f77832c55dd805f1
Tony Miller <mcfiredrill@gmail.com>
2019-11-08 16:16:24 +0900

    add logout to user menu

--- app/components/user-menu.js
+++ app/components/user-menu.js
@@ -1,9 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNameBindings: ['showing:block:hidden'],
+  classNameBindings: ['showingClass'],
   session: service(),
+  showingClass: computed('showing', 'isShowingMobileMenu', function(){
+    if(this.showing){
+      return "sm:block";
+    }else{
+      return "sm:hidden";
+    }
+  }),
   actions: {
     logout(){
       this.session.invalidate();
