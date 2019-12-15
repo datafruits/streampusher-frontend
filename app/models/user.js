b31dcec729441830a72ec6ef3f7ce6f6365dcdac
Tony Miller <mcfiredrill@gmail.com>
2019-12-15 15:23:46 +0900

    add djs form

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
 const { Model } = DS;
+import { computed } from '@ember/object';
 
 export default Model.extend({
   username: DS.attr(),
   email: DS.attr(),
   role: DS.attr(),
   timeZone: DS.attr(),
+  bio: DS.attr(),
+  isAdmin: computed('role', function() {
+    return this.role == 'admin';
+  })
 });
