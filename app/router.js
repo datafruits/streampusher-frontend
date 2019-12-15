11c5c337ae9be8306bef574c9a03876917ebfc2a
Tony Miller <mcfiredrill@gmail.com>
2019-12-15 15:24:43 +0900

    add admin route

--- app/router.js
+++ app/router.js
@@ -23,6 +23,7 @@ Router.map(function() {
     this.route('profile');
     this.route('settings');
     this.route('radio-settings');
+    this.route('admin');
 
     this.route('blog-posts', function() {
       this.route('show', {path: '/:id'});
