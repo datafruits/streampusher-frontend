8b9d41067b0db9e99030ad77eaf065786c3e79e8
Tony Miller <mcfiredrill@gmail.com>
2019-12-15 15:24:33 +0900

    add blog-post title

--- app/models/blog-post.js
+++ app/models/blog-post.js
@@ -1,6 +1,10 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 const { Model } = DS;
 
 export default Model.extend({
   blogPostBodies: DS.hasMany('blog-post-body'),
+  title: computed('blogPostBodies.[]', function(){
+    return this.blogPostBodies.firstObject.title;
+  })
 });
