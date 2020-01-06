743203b53a05a772de077a62bda7fb1d01c86916
Tony Miller <mcfiredrill@gmail.com>
2020-01-06 22:31:36 +0900

    add flash messages

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,11 +3,15 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   session: service(),
+  flashMessages: service(),
   actions: {
     signIn(){
       let login = this.login;
       let password = this.password;
-      this.session.authenticate('authenticator:devise', login, password).catch((reason) => {
+      this.session.authenticate('authenticator:devise', login, password).then(() => {
+        this.flashMessages.success('Logged in!');
+      }).catch((reason) => {
+        this.flashMessages.danger("Incorrect username or password");
         console.log(reason);
       });
     }
