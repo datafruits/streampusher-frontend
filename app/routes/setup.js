77e3e04327af028f452d2859eaf20c117909b8b4
Tony Miller <mcfiredrill@gmail.com>
2020-01-26 20:47:58 +0900

    add setup

--- app/routes/setup.js
+++ app/routes/setup.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import ENV from "streampusher-frontend/config/environment";
 
 export default Route.extend({
   flashMessages: service(),
@@ -8,5 +9,16 @@ export default Route.extend({
     if(this.session.isAuthenticated){
       this.transitionTo('authenticated.dashboard');
     }
+    fetch(`${ENV.API_HOST}/setup/allowed.json`).then((response) => {
+      if(response.ok){
+        console.log('setup is allowed');
+      }else{
+        console.log('not allowed');
+        this.transitionTo('login');
+      }
+    }).catch((error) => {
+      console.log(`some error requesting setup allowed: ${error}`);
+      this.transitionTo('login');
+    });
   }
 });
