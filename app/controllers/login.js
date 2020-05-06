2576bde8a7398fea7c9ceb69600ac55c7e54844d
Tony Miller <mcfiredrill@gmail.com>
2020-05-06 16:22:10 -0700

    fix rememberMe to login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,5 +1,6 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
 
 export default class LoginController extends Controller {
@@ -9,11 +10,14 @@ export default class LoginController extends Controller {
   @service
   flashMessages;
 
+  @tracked rememberMe = true;
+
   @action
   signIn() {
     let login = this.login;
     let password = this.password;
     this.session.authenticate('authenticator:devise', login, password).then(() => {
+      this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
       this.flashMessages.success('Logged in!');
       this.transitionToRoute('authenticated.dashboard');
     }).catch((reason) => {
