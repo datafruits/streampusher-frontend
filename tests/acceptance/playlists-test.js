64014d8c8cb4b71081727f46109d4fb7914cfc59
Tony Miller <mcfiredrill@gmail.com>
2020-12-23 18:54:52 +0900

    some more tests i'll need to write...

--- tests/acceptance/playlists-test.js
+++ tests/acceptance/playlists-test.js
@@ -1,40 +1,53 @@
-import { module, test } from 'qunit';
-import { visit, currentURL, click, fillIn, pauseTest } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
-import { authenticateSession } from 'ember-simple-auth/test-support';
-import { setupMirage } from 'ember-cli-mirage/test-support';
+import { module, test } from "qunit";
+import {
+  visit,
+  currentURL,
+  click,
+  fillIn,
+  pauseTest,
+} from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+import { authenticateSession } from "ember-simple-auth/test-support";
+import { setupMirage } from "ember-cli-mirage/test-support";
 
-module('Acceptance | playlists', function(hooks) {
+module("Acceptance | playlists", function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting /playlists', async function(assert) {
+  test("visiting /playlists", async function (assert) {
     await authenticateSession();
-    await visit('/playlists');
+    await visit("/playlists");
 
-    assert.equal(currentURL(), '/playlists');
+    assert.equal(currentURL(), "/playlists");
   });
-  test('create a new playlist', async function(assert) {
+  test("create a new playlist", async function (assert) {
     await authenticateSession();
-    await visit('/playlists');
+    await visit("/playlists");
 
-    assert.equal(currentURL(), '/playlists');
+    assert.equal(currentURL(), "/playlists");
 
     await pauseTest();
     await click("[data-test-new-playlist-button]");
     await fillIn("input#playlist-name", "my new playlist");
     await click("[data-test-save-playlist-button]");
-    assert.equal(document.querySelector("[data-current-playlist]").textContent.includes("my new playlist"), true);
+    assert.equal(
+      document
+        .querySelector("[data-current-playlist]")
+        .textContent.includes("my new playlist"),
+      true
+    );
   });
 
-  test('uploading a new track', async function(assert) {
-  });
+  test("uploading a new track", async function (assert) {});
 
-  test('add track to playlist', async function(assert) {
-  });
+  test("add track to playlist", async function (assert) {});
 
-  test('editing a track', async function(assert) {
+  test("editing a track", async function (assert) {
     // set artwork
     // set scheduled show
   });
+
+  test("edit playlist settings", async function (assert) {});
+  test("edit playlist name", async function (assert) {});
+  test("navigate to a different playlist", async function (assert) {});
 });
