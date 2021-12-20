e4213cb0ab83b11af44d7c2e02dcb40f160517ec
Tony Miller <mcfiredrill@gmail.com>
2021-12-19 23:44:15 -0800

    fix scheduled show form

--- app/components/scheduled-show/form.js
+++ app/components/scheduled-show/form.js
@@ -5,6 +5,9 @@ import { inject as service } from "@ember/service";
 import { debounce } from "@ember/runloop";
 import RSVP from "rsvp";
 import moment from 'moment';
+import Changeset from "ember-changeset";
+import lookupValidator from "ember-changeset-validations";
+import ScheduledShowValidations from "../../validations/scheduled-show";
 
 export default class ScheduledShowForm extends Component {
   @tracked isSaving = false;
@@ -36,8 +39,17 @@ export default class ScheduledShowForm extends Component {
     },
   ];
 
+  constructor() {
+    super(...arguments);
+    this.changeset = new Changeset(
+      this.args.model,
+      lookupValidator(ScheduledShowValidations),
+      ScheduledShowValidations
+    );
+  }
+
   get formattedDay() {
-    return moment(this.args.changeset.start).format('dddd MMMM Do YYYY');
+    return moment(this.changeset.start).format('dddd MMMM Do YYYY');
   }
 
   @action
@@ -47,41 +59,42 @@ export default class ScheduledShowForm extends Component {
 
   @action
   setRecurringInterval(interval) {
-    this.args.changeset.set("recurringInterval", interval);
+    this.changeset.set("recurringInterval", interval);
   }
 
   @action
   setPlaylist(playlist) {
-    this.args.changeset.set("playlist", playlist);
+    this.changeset.set("playlist", playlist);
   }
 
   @action
   setStart(time){
     let hours = time.split(':')[0];
     let minutes = time.split(':')[1];
-    const oldDate = this.args.changeset.start;
+    const oldDate = this.changeset.start;
     let newDate = new Date(oldDate.getFullYear(), oldDate.getMonth(), oldDate.getDate(), hours, minutes);
-    this.args.changeset.set("start", newDate);
+    this.changeset.set("start", newDate);
   }
 
   @action
   setEnd(time){
     let hours = time.split(':')[0];
     let minutes = time.split(':')[1];
-    const oldDate = this.args.changeset.end;
+    const oldDate = this.changeset.end;
     let newDate = new Date(oldDate.getFullYear(), oldDate.getMonth(), oldDate.getDate(), hours, minutes);
-    this.args.changeset.set("end", newDate);
+    this.changeset.set("end", newDate);
   }
 
   @action
   setHosts(djs){
-    this.args.changeset.set('djs', djs);
+    this.changeset.set('djs', djs);
   }
 
   @action
-  save() {
+  save(event) {
+    event.preventDefault();
     this.isSaving = true;
-    let show = this.args.changeset;
+    let show = this.changeset;
     //let currentPlaylist = this.get('currentPlaylist.playlist');
     const onSuccess = () => {
       this.isSaving = false;
@@ -102,6 +115,7 @@ export default class ScheduledShowForm extends Component {
       debounce(this, this._performDjsSearch, term, resolve, reject, 600);
     });
   }
+
   _performDjsSearch(term, resolve, reject) {
     this.store.query("user", {
       search: {
