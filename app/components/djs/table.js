92a648c92703eb100dbac6670608435f4a14eb06
Tony Miller <mcfiredrill@gmail.com>
2022-03-01 20:42:14 -0800

    use FruitsUi::Pagination component
    * switch ember concurrency pattern to Await

--- app/components/djs/table.js
+++ app/components/djs/table.js
@@ -1,6 +1,16 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class DjsTableComponent extends Component {
   @service currentUser;
+
+  @service store;
+
+  @action
+  fetchDjs() {
+    const query = { page: this.args.searchParams.page, search: { keyword: this.args.searchParams.query } };
+    let djsPromise = this.store.query('user', query);
+    return djsPromise;
+  }
 }
