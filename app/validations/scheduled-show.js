9593f32ba72d296faf5080f37e9524fdf6c1c1ba
Tony Miller <mcfiredrill@gmail.com>
2022-04-08 10:35:58 -0700

    require guest is isGuest is checked

--- app/validations/scheduled-show.js
+++ app/validations/scheduled-show.js
@@ -3,4 +3,5 @@ import { validatePresence } from 'ember-changeset-validations/validators';
 export default {
   title: validatePresence(true),
   playlist: validatePresence(true),
+  guest: validatePresence({ presence: true, on: 'isGuest'})
 };
