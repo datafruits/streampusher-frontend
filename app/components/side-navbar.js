d81b5a87541740886a5f25d9532d0cec2aa19224
Tony Miller <mcfiredrill@gmail.com>
2022-05-01 19:48:57 +0900

    fix side-navbar

--- app/components/side-navbar.js
+++ app/components/side-navbar.js
@@ -1,6 +1,10 @@
+import { classNameBindings } from '@ember-decorators/component';
 import { inject as service } from '@ember/service';
-import Component from '@glimmer/component';
+import Component from '@ember/component';
+import classic from 'ember-classic-decorator';
 
+@classic
+@classNameBindings('isShowingMobileMenu:block:hidden')
 export default class SideNavbar extends Component {
   @service
   currentRadio;
