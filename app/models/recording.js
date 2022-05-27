158818fdb5f69a14e868abef2ab1a73a04121034
Tony Miller <mcfiredrill@gmail.com>
2022-05-27 11:30:39 +0900

    add DL links back to recordings
    pretty hacky but might work for now

--- app/models/recording.js
+++ app/models/recording.js
@@ -1,9 +1,12 @@
 import Model, { attr } from '@ember-data/model';
+import { inject as service } from '@ember/service';
+import ENV from 'streampusher-frontend/config/environment';
 
 export default class Recording extends Model {
   @attr filesize;
   @attr path;
   @attr processingStatus;
+  @service session;
 
   get unprocessed() {
     return this.processingStatus === 'unprocessed';
@@ -16,5 +19,9 @@ export default class Recording extends Model {
   }
   get processingFailed() {
     return this.processingStatus === 'processing_failed';
+  }
+
+  get downloadLink() {
+    return `${ENV.API_HOST}/recordings/${this.id}?token=${this.session.data.authenticated.token}`;
   }
 }
