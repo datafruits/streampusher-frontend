e0bee8e1b1529e7e554a1794e102a6701c6ede1d
Tony Miller <mcfiredrill@gmail.com>
2022-08-02 12:17:21 -0700

    disable purgecss for now

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const isProduction = EmberApp.env() === 'production';
+//const isProduction = EmberApp.env() === 'production';
 
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // add extra paths here for components/controllers which include tailwind classes
-      './app/index.html',
-      './app/templates/**/*.hbs',
-      './app/components/**/*.hbs',
-    ],
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  },
-};
+// const purgeCSS = {
+//   module: require('@fullhuman/postcss-purgecss'),
+//   options: {
+//     content: [
+//       // add extra paths here for components/controllers which include tailwind classes
+//       './app/index.html',
+//       './app/templates/**/*.hbs',
+//       './app/components/**/*.hbs',
+//     ],
+//     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+//   },
+// };
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -30,7 +30,7 @@ module.exports = function (defaults) {
           },
 
           require('tailwindcss')('./app/tailwind/config.js'),
-          ...(isProduction ? [purgeCSS] : []),
+          //...(isProduction ? [purgeCSS] : []),
         ],
       },
     },
