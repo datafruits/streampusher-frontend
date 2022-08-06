5f438f8fc256405ebc07f2f41e0592afe24b74e5
Tony Miller <mcfiredrill@gmail.com>
2022-08-06 11:47:44 -0700

    add acceptance tests back

---
+++ tests/acceptance/recordings-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | recordings', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('publishing a recording', async function (assert) {
+    await visit('/recordings');
+
+    assert.equal(currentURL(), '/recordings');
+  });
+});
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,11 +1,10 @@
-import Application from 'frontend/app';
-import config from 'frontend/config/environment';
+import Application from 'streampusher-frontend/app';
+import config from 'streampusher-frontend/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import './helpers/flash-message';
-
 
 setApplication(Application.create(config.APP));
 
