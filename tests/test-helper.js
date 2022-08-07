7a22c20c3859bde0c8fa7337d938c534cf1779e7
Tony Miller <mcfiredrill@gmail.com>
2022-08-07 13:09:44 -0700

    Revert "add acceptance tests back"
    This reverts commit 5f438f8fc256405ebc07f2f41e0592afe24b74e5.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,10 +1,11 @@
-import Application from 'streampusher-frontend/app';
-import config from 'streampusher-frontend/config/environment';
+import Application from 'frontend/app';
+import config from 'frontend/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import './helpers/flash-message';
+
 
 setApplication(Application.create(config.APP));
 
