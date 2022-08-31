6d6e07cc0a90152f7dc56fc8c46d9d8380cfdcf5
Tony Miller <mcfiredrill@gmail.com>
2022-08-31 09:01:17 -0700

    fix tracks sorting/uploading

--- app/components/tracks/list.js
+++ app/components/tracks/list.js
@@ -2,6 +2,7 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
 
 export default class TracksList extends Component {
   @service
@@ -12,6 +13,7 @@ export default class TracksList extends Component {
 
   @tracked tracks = [];
   @tracked tracksQuery;
+  @tracked meta;
 
   // FIXME sort with @tracked instead??
   // @sort('tracks', function(a, b){
@@ -33,6 +35,28 @@ export default class TracksList extends Component {
   //   }
   // })
   // sortedTracks;
+  //
+  get sortedTracks() {
+    return this.tracks.sort((a, b) => {
+      if(a.isUploading || b.isUploading){
+        if(a.isUploading && b.isUploading){
+          return 0;
+        } else if(a.isUploading){
+          return -1;
+        } else if(b.isUploading){
+          return 1;
+        }
+      } else {
+        if(a.get('createdAt') === b.get('createdAt')){
+          return 0;
+        } else if(a.get('createdAt') > b.get('createdAt')){
+          return -1;
+        } else if(a.get('createdAt') < b.get('createdAt')){
+          return 1;
+        }
+      }
+    });
+  }
 
   @action
   pushTrack(track) {
@@ -40,12 +64,22 @@ export default class TracksList extends Component {
   }
 
   @action
-  fetchTracks() {
+  performTask() {
+    this.fetchTracks.perform();
+  }
+
+  @(task(function* () {
+    yield timeout(1000);
     const query = {
       page: this.args.searchParams.page,
       search: { keyword: this.args.searchParams.tracksQuery },
     };
-    let tracksPromise = this.store.query('track', query);
-    return tracksPromise;
-  }
+    let tracks = this.store.query('track', query).then((tracks) => {
+      this.meta = tracks.meta;
+      return tracks;
+    });
+    let resolvedTracks = yield tracks;
+    return (this.tracks = resolvedTracks.toArray());
+  }).restartable())
+  fetchTracks;
 }
