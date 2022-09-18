cce775b3f5d8381381a73e38a3fa967c84dd7dc9
Tony Miller <mcfiredrill@gmail.com>
2022-09-18 12:41:31 -0700

    only run not-in-past validator on new shows

--- app/validators/not-in-past.js
+++ app/validators/not-in-past.js
@@ -1,10 +1,14 @@
 export default function notInPast() {
-  return (key, newValue /*, oldValue, changes, content*/) => {
-    const now = new Date();
-    if (newValue > now) {
-      return true;
+  return (key, newValue , oldValue, changes, content) => {
+    if(content.isNew) {
+      const now = new Date();
+      if (newValue > now) {
+        return true;
+      } else {
+        return `${key} cannot be in the past`;
+      }
     } else {
-      return `${key} cannot be in the past`;
+      return true;
     }
   };
 }
