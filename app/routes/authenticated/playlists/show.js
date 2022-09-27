f4d71d00210939456d04c43a7929eb9997dce32b
Tony Miller <mcfiredrill@gmail.com>
2022-09-26 21:41:17 -0700

    set history in playlists/show

--- app/routes/authenticated/playlists/show.js
+++ app/routes/authenticated/playlists/show.js
@@ -1,8 +1,11 @@
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default class ShowRoute extends Route {
+  @service history;
+
   @action
   transitionAfterDelete() {
     this.store.findAll('playlist').then((playlists) => {
@@ -16,5 +19,11 @@ export default class ShowRoute extends Route {
       playlist: this.store.findRecord('playlist', params.id),
       labels: this.store.loadRecords('label'),
     });
+  }
+
+  afterModel() {
+    console.log('setting history');
+    console.log(this.fullRouteName);
+    this.history.previousRoute = this.fullRouteName;
   }
 }
